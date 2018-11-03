9861cb5fcf74878ac479f9db6715760bf003c7ca
Elijah <elijahelrod@mail.usf.edu>
2018-11-03 10:08:14 -0700

    UploadScreen

---
+++ screens/UploadScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default class LinksScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Links',
+  };
+
+  render() {
+    return (
+      <View>
+          <Text> Hello</Text>
+      </View>
+    );
+  }
+}
